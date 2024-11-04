1103b4039e37dff938180a2d95b032ff55976293
Eldar Hasanov <eldarh079@berkeley.edu>
2024-11-04 00:19:16 -0800

    added autograding and new questions

--- elements/pl-graph-constructor/pl-graph-constructor.py
+++ elements/pl-graph-constructor/pl-graph-constructor.py
@@ -39,11 +39,6 @@ def parse(element_html, data):
     graph_data = data['submitted_answers'].get('graphData', '')
     data['submitted_answers']['graphData'] = graph_data
 
-def grade(element_html, data):
-    # Implement grading logic here
-    # For now, we'll give full credit for any submission
-    data['score'] = 1
-    student_dot = data['submitted_answers'].get('graphData', '')
-    print(student_dot)
+
 
 
