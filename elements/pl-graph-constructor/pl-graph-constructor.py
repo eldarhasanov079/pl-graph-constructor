358bb2a279aea300e2f59e1528061f4f46c4d779
chris-rau <chrisrau289@gmail.com>
2024-12-06 12:59:58 -0800

    Added answers-name

--- elements/pl-graph-constructor/pl-graph-constructor.py
+++ elements/pl-graph-constructor/pl-graph-constructor.py
@@ -10,7 +10,7 @@ ALLOW_NODEMARKING_DEFAULT = True
 
 def prepare(element_html, data):
     element = lxml.html.fragment_fromstring(element_html)
-    required_attribs = ["directed"]
+    required_attribs = ["directed", "answers-name"]
     optional_attribs = ["allow-selflinks", "multigraph", "allow-nodemarking"]
     
     pl.check_attribs(element, required_attribs, optional_attribs)
@@ -21,19 +21,21 @@ def render(element_html, data):
     #return chevron.render(element_html, {}).strip()
     element = lxml.html.fragment_fromstring(element_html)
     directed = pl.get_boolean_attrib(element, "directed")
+    answers_name = pl.get_string_attrib(element, "answers-name")
     allow_selflinks = pl.get_boolean_attrib(element, "allow-selflinks", ALLOW_SELFLINKS_DEFAULT)
     multigraph = pl.get_boolean_attrib(element, "multigraph", ALLOW_SELFLINKS_DEFAULT)
     allow_nodemarking = pl.get_boolean_attrib(element, "allow-nodemarking", ALLOW_NODEMARKING_DEFAULT)
 
     config = {
+        'answers_name': answers_name,
         'directed': "true" if directed else "false",
         'allow_selflinks': "true" if allow_selflinks else "false",
         'multigraph': "true" if multigraph else "false",
         'allow_nodemarking': "true" if allow_nodemarking else "false"
     }
-    
+        
     with open('pl-graph-constructor.mustache', 'r') as f:
-        return chevron.render(f, {'config': config}).strip()
+        return chevron.render(f, {'config_py': config}).strip()
 
 def parse(element_html, data):
     graph_data = data['submitted_answers'].get('graphData', '')
